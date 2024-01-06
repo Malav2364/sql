2d64aff843132883c33c1ae45f92ffd40e4a0ad7
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2024-01-06 23:12:20 +0530

    sql1pt

--- prac.sql
+++ prac.sql
@@ -1,5 +1,8 @@
-create table emp (emp_name VARCHAR(20), dob DATE);
-INSERT INTO emp VALUES ('Malav', TO_DATE('23-06-2004', 'DD-MM-YYYY'));
-INSERT INTO emp VALUES ('Aayush', TO_DATE('28-03-1969', 'DD-MM-YYYY'));
-INSERT INTO emp VALUES ('Mayur', TO_DATE('28-04-1969', 'DD-MM-YYYY'));
-SELECT * FROM emp where dob = to_date('23-06-2004', 'DD-MM-YYYY');
+-- SQL(Structure Query Language)
+CREATE TABLE st1 (sr_no NUMBER(10), st_nm VARCHAR(20), age NUMBER(10), dept VARCHAR(20))
+INSERT INTO st1 VALUES (1, 'Suresh', 18, 'Computer Science');
+INSERT INTO st1 VALUES (2, 'Mahesh', 19, 'IT');
+INSERT INTO st1 VALUES (3, 'Ramesh', 20, 'Civil Engineering');
+INSERT INTO st1 VALUES (4, 'kamlesh', 19, 'Architecture');
+INSERT INTO st1 VALUES (5, 'Vimlesh', 19, 'Mechnical');
+select * from st1;
